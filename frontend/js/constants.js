53291dc505326086c7d14d47b4d3617f1cd6ef95
ttaszi <116489332+ttaszi@users.noreply.github.com>
2022-12-29 16:12:42 +0100

    Add files via upload

---
+++ frontend/js/constants.js
@@ -0,0 +1,22 @@
+const contractAddress = "0xa67795c2Fb8D62CceBA9B5d796b87d54511F6C35"; // Replace with your own contract address
+const chain = 'polygon'; // goerli, polygon, or ethereum
+
+const welcome_h1 = "Welcome to the worldchips NFT Project!!";
+const welcome_h2 = "Connect to MetaMask to Get Started";
+const welcome_p = 'The worldchips NFT project is a collection of cool places around the World, each painted in 14 different styles.';
+const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
+const h1_presale_mint = "Pre-Sale Minting Open!!";
+const h1_public_mint = "Public Minting Open!!";
+const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
+const h2_presale_mint = "Public Minting Countdown";
+const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
+const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
+const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
+const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
+const p_public_mint = "No whitelist needed. Public minting is now open! 🎉";
+const button_presale_coming_soon = "Get on the Whitelist";
+const button_presale_mint_whitelisted = "Mint Your Special NFT";
+const button_presale_mint_not_whitelisted = "Get on the Whitelist";
+const button_presale_already_minted = "Join The Community";
+const button_public_mint = "Mint Your NFT";
+const mint_failed = "Minting failed. 😢 Please try again.";
